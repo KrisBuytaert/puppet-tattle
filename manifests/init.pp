2d506cac88a731d142b434a99646926c015af6a3
Kris Buytaert <sdog@inuits.be>
2012-02-15 23:19:39 +0100

    Puppetlint is now happy

--- manifests/init.pp
+++ manifests/init.pp
@@ -22,14 +22,14 @@ class tattle ($dbhost = 'localhost',
 )
 {
 
-  package {"tattle":
+  package {'tattle':
     ensure => present;
   }
 
   file { '/etc/httpd/conf.d/tattle.conf':
     ensure => 'file',
     group  => '0',
-    mode   => '644',
+    mode   => '0644',
     owner  => '0',
     source => 'puppet:///modules/tattle/tattle.conf',
   }
@@ -37,14 +37,14 @@ class tattle ($dbhost = 'localhost',
   file { '/var/vhosts/tattle/html/tmp':
     ensure => 'directory',
     group  => '0',
-    mode   => '777',
+    mode   => '0777',
     owner  => '0',
   }
 
   file { '/var/vhosts/tattle/html/inc/config.php':
     ensure   => 'file',
     group    => '0',
-    mode     => '664',
+    mode     => '0664',
     owner    => '0',
     content  => template('tattle/config.php.erb')
   }
